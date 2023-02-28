bedcb44f0188326b55ae85d77f5d15fc8b0193e0
narczi <smiechu47@gmail.com>
2023-02-28 20:20:42 +0100

    transformacja wektoru

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <fstream>
 #include "Shader.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -231,6 +235,12 @@ int main() {
 
 	SquareShader.setFloat("mixer", 0.2f);
 	
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f); // zdefiniowanie macierzy jednostkowej
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+
+	std::cout << vec.x << ", " << vec.y << ", " << vec.z << std::endl;
 
 	while (!glfwWindowShouldClose(window)) 
 	{
